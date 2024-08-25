6acdbc20bbb92dd38976e176972abda619459b54
Phạm Hoàng Minh Anh <anhphmse183496@fpt.edu.vn>
2024-08-25 23:41:13 +0700

    Config SwaggerUI

--- BeanJournal_BackEnd/Program.cs
+++ BeanJournal_BackEnd/Program.cs
@@ -183,11 +183,12 @@ builder.Services.AddScoped<IUserRepository, UserRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-  app.UseSwagger();
-  app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+
+//}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
