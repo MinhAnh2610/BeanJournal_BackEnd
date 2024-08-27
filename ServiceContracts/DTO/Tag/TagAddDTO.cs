5b49b4e7de22185e6a6b87c444de94d8b4efc017
Phạm Hoàng Minh Anh <anhphmse183496@fpt.edu.vn>
2024-08-27 20:18:08 +0700

    Update TagDtos

--- ServiceContracts/DTO/Tag/TagAddDTO.cs
+++ ServiceContracts/DTO/Tag/TagAddDTO.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using ServiceContracts.ValidationAttributes;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -12,5 +14,11 @@ namespace ServiceContracts.DTO.Tag
     [Required]
     [StringLength(50)]
     public string Name { get; set; } = string.Empty;
+    [Required]
+    [ImageFile(new string[] { ".png", ".svg", ".gif", ".jpeg", ".jpg", ".tiff", ".psd", ".eps", ".raw" })]
+    public IFormFile? Image { get; set; }
+    [Required]
+    [ImageFile(new string[] { ".png", ".svg", ".gif", ".jpeg", ".jpg", ".tiff", ".psd", ".eps", ".raw" })]
+    public IFormFile? Icon { get; set; }
   }
 }
