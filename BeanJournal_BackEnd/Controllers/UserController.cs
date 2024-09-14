9e573f304980cf4fede214f9c558c9a578522739
Phạm Hoàng Minh Anh <anhphmse183496@fpt.edu.vn>
2024-09-14 23:50:01 +0700

    User profile API

---
+++ BeanJournal_BackEnd/Controllers/UserController.cs
@@ -0,0 +1,40 @@
+﻿using Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BeanJournal_BackEnd.Controllers
+{
+    /// <summary>
+    /// User API
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="userManager"></param>
+        public UserController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        /// <summary>
+        /// Get user profile
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("user-profile")]
+        public async Task<IActionResult> GetUserProfile()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return BadRequest("User not found");
+            }
+            return Ok(user);
+        }
+    }
+}
