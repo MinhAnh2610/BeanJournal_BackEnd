9aa76793eb89857ccb6eb225bc25c3fcb51f3208
Phạm Hoàng Minh Anh <anhphmse183496@fpt.edu.vn>
2024-10-13 00:51:22 +0700

    Comment in helper class

--- BeanJournal_BackEnd/Helpers/DatabaseManagementService.cs
+++ BeanJournal_BackEnd/Helpers/DatabaseManagementService.cs
@@ -3,6 +3,9 @@ using Microsoft.EntityFrameworkCore;
 
 namespace BeanJournal_BackEnd.Helpers
 {
+	/// <summary>
+	/// Helper class to initalize database for docker compose up
+	/// </summary>
 	public class DatabaseManagementService
 	{
 		/// <summary>
