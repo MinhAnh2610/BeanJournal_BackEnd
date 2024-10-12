68d95f267bcb3a5dca78e2a03c45e340144c79ef
Phạm Hoàng Minh Anh <anhphmse183496@fpt.edu.vn>
2024-10-12 23:53:06 +0700

    New Migration and Database migration for Docker Container

---
+++ BeanJournal_BackEnd/Helpers/DatabaseManagementService.cs
@@ -0,0 +1,20 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BeanJournal_BackEnd.Helpers
+{
+	public class DatabaseManagementService
+	{
+		/// <summary>
+		/// Migration initialization
+		/// </summary>
+		/// <param name="app"></param>
+		public static void MigrationInitalization(IApplicationBuilder app)
+		{
+			using (var serviceScope = app.ApplicationServices.CreateScope())
+			{
+				serviceScope.ServiceProvider.GetService<ApplicationDbContext>()!.Database.Migrate();
+			}
+		}
+	}
+}
